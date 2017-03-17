6f4c3ee8d5936cc3a56d7178bfa14cc6eaa028cf
Charles Peterson <nowledster@gmail.com>
2017-03-17 20:03:41 +0300

    New Working Branch

---
+++ scripts/utilities.js
@@ -0,0 +1,10 @@
+var forEach  =  function(point){
+    
+   var points = document.getElementsByClassName('point');
+    
+      for(var i = 0; i< points.length; i++){
+          forEach(points[i]);
+          
+      }
+  
+}
