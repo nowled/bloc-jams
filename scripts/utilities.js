7a0e5dbd14e2c965c2813ac95a95912e444ccdd9
Charles Peterson <nowledster@gmail.com>
2017-03-17 22:39:02 +0300

    last changes to MASTER in Checkpoint 9

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,9 +1,6 @@
+function forEach(arry, callbackFunc) {
+    for (var i = 0; i < arry.length; i++) {
+        callbackFunc(arry[i]);
 
-
-function forEach(arry, callbackFunc){
-      for(var i = 0; i< arry.length; i++){
-          callbackFunc(arry[i]);
-          
-      }
-  
+    }
 }
