4a7a005c0b5962202ea8c8bf2a86c7eaea81577c
Charles Peterson <nowledster@gmail.com>
2017-03-17 22:42:37 +0300

    Enhanced and last checkpoint9-assignment work

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,10 +1,7 @@
-function forEach(arry, callbackFunc){
-    
-   var points = document.getElementsByClassName('point');
-    
-      for(var i = 0; i< points.length; i++){
-          callbackFunc(arry[i]);
-          
-      }
-  
+function forEach(arry, callbackFunc) {
+
+    for (var i = 0; i < arry.length; i++) {
+        callbackFunc(arry[i]);
+    }
+
 }
