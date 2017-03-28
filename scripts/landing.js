c410b747daf01c801e5c1c1fdfbe812641f2b479
Charles Peterson <nowledster@gmail.com>
2017-03-28 17:40:54 +0300

    End Of Checkpoint16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,47 @@
- var pointsArray = document.getElementsByClassName('point');
+// var pointsArray = document.getElementsByClassName('point');
 
- var revealPoint = function (point) {
+ /**var animatePoints = function (points) {
+     forEach(points, revealPoint);
+ };
+**/
+var animatePoints = function() {
+    
+    var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
+    
+    $.each($('.point'), revealPoint);
+ };
+/** var revealPoint = function (point) {
      point.style.opacity = 1;
      point.style.transform = "scaleX(1) translateY(0)";
      point.style.msTransform = "scaleX(1) translateY(0)";
      point.style.WebkitTransform = "scaleX(1) translateY(0)";
 
- };
- var animatePoints = function (points) {
-     forEach(points, revealPoint);
-
- };
+ }; **/
 
 
- window.onload = function () {
+ $(window).load(function() {
      // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+      if($(window).height() > 950){
+         animatePoints();
      }
-
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     /** var sellingPoints = document.getElementsByClassName('selling-points')[0];
      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+      refactoring */
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-     window.addEventListener('scroll', function (event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+   //  window.addEventListener('scroll', function (event) {
+          $(window).scroll(function(event) {
+       /**  if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray); **/
+                if($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
      });
- }
+                
+                });
