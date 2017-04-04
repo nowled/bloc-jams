0990cd09128860c857ba272390a83359dc79343a
Charles Peterson <nowledster@gmail.com>
2017-04-04 19:36:16 +0300

    Checkpoint17

--- scripts/album.js
+++ scripts/album.js
@@ -96,31 +96,42 @@ var createSongRow = function (songNumber, songName, songLength) {
                           to store things */
         '<tr class="album-view-song-item">' + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
 
-    return template;
+      return $(template);
 };
 
 
 //I put the albums in a global scope so I can access the album image on
 //windows onload where I will put my event listener
+/** 
 var albumTitle = document.getElementsByClassName('album-view-title')[0];
 var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
 var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+**/
+
+    var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
 
 //Here set values to each part of the album
 var setCurrentAlbum = function (album) {
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
+    $albumTitle.text(album.title);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
 
     // clear list of album song list container
-    albumSongList.innerHTML = '';
+   // albumSongList.innerHTML = '';
+     $albumSongList.empty();
 
     // Build List of Songs from Album
     for (var i = 0; i < album.songs.length; i++) {
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+       /** albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration); **/
+        var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
     }
 };
 
--- scripts/collection.js
+++ scripts/collection.js
@@ -1,6 +1,7 @@
 //You can take this file and run it on pages without hardcoding it in the page
 //This will Generate our Albums Templates
-var collectionItemTemplate =
+    var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -15,17 +16,24 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
+        
+         return $(template);
+ };
 
 //You can display as many items as you want to on a given page without hard coding it 
-window.onload = function() {
+  $(window).load(function() {
      // #1 This will load on startup
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+     //var collectionContainer = document.getElementsByClassName('album-covers')[0];
+        var $collectionContainer = $('.album-covers');
      // #2 This will grab all of our 'album-covers' classes on the collections.html page
-     collectionContainer.innerHTML = '';
+    // collectionContainer.innerHTML = '';
+        $collectionContainer.empty();
  
      // #3 After we grab the class we will set the variable container to be empty so i can
     //hold our collections Templates above(1-16)
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+          var $newThumbnail = buildCollectionItemTemplate();
+         // #5
+         $collectionContainer.append($newThumbnail);
      }
- }
+ });
